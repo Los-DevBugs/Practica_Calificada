b22d180635d48adb63c018286846135ce120eb68
Bill-botchat <bilrodrick7@gmail.com>
2024-10-07 15:50:24 -0500

    Actualizando

---
+++ CapaPresentacion/Intranet/frmIntranet.aspx
@@ -0,0 +1,135 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="frmIntranet.aspx.cs" Inherits="CapaPresentacion.Intranet.frmIntranet" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:HiddenField ID="hdnActiveTabIndex" runat="server" />
+    
+ <ul class="nav nav-tabs" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a class="nav-link active" id="simple-tab-0" data-bs-toggle="tab" href="#simple-tabpanel-0" role="tab" aria-controls="simple-tabpanel-0" aria-selected="true">Alumno</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="simple-tab-1" data-bs-toggle="tab" href="#simple-tabpanel-1" role="tab" aria-controls="simple-tabpanel-1" aria-selected="false">Docente</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="simple-tab-2" data-bs-toggle="tab" href="#simple-tabpanel-2" role="tab" aria-controls="simple-tabpanel-2" aria-selected="false">Asignatura</a>
+  </li>
+</ul>
+    
+<div class="tab-content pt-5" id="tab-content">
+  <div class="tab-pane fade active" id="simple-tabpanel-0" role="tabpanel" aria-labelledby="simple-tab-0"><h3>Mantimiento de la tabla Alumno</h3>
+<p>
+    CodAlumno : <asp:TextBox runat="server" ID="txtCodAlumno" />
+</p>
+<p>
+    APaterno : <asp:TextBox runat="server" ID="txtAPaternoAlumno" />
+</p>
+<p>
+    AMaterno : <asp:TextBox runat="server" ID="txtAMaternoAlumno" />
+</p>
+<p>
+    Nombres : <asp:TextBox runat="server" ID="txtNombresAlumno" />
+</p>
+<p>
+    Usuario : <asp:TextBox runat="server" ID="txtUsuarioAlumno" />
+</p>
+<p>
+    CodCarera: <asp:TextBox runat="server" ID="txtCodCarrera" />
+</p>
+<p>
+    <asp:Button Text="Agregar" runat="server" ID="btnAgregarAlumno" OnClick="btnAgregarAlumno_Click" />
+    <asp:Button Text="Eliminar" runat="server" ID="btnEliminarAlumno" OnClick="btnEliminarAlumno_Click" />
+    <asp:Button Text="Actualizar" runat="server" ID="btnActualizarAlumno" OnClick="btnActualizarAlumno_Click" />
+</p>
+<p>
+    Buscar : <asp:TextBox runat="server" ID="txtBuscarAlumno" />
+    <asp:Button Text="Buscar" runat="server" ID="btnBuscarAlumno" OnClick="btnBuscarAlumno_Click" />
+</p>
+<p>
+    <asp:GridView runat="server" ID="gvAlumno"></asp:GridView>
+</p></div>
+  <div class="tab-pane fade" id="simple-tabpanel-1" role="tabpanel" aria-labelledby="simple-tab-1"><h3>Mantimiento de la tabla Docente</h3>
+<p>
+    CodDocen : <asp:TextBox runat="server" ID="txtCodDocente" />
+</p>
+<p>
+    APaterno : <asp:TextBox runat="server" ID="txtAPaternoDocente" />
+</p>
+<p>
+    AMaterno : <asp:TextBox runat="server" ID="txtAMaternoDocente" />
+</p>
+<p>
+    Nombres : <asp:TextBox runat="server" ID="txtNombresDocente" />
+</p>
+<p>
+    Usuario: <asp:TextBox runat="server" ID="txtUsuarioDocente" />
+</p>   
+<p>
+    <asp:Button Text="Agregar" runat="server" ID="btnAgregarDocente" OnClick="btnAgregarDocente_Click"/>
+    <asp:Button Text="Eliminar" runat="server" ID="btnEliminarDocente" OnClick="btnEliminarDocente_Click"/>
+    <asp:Button Text="Actualizar" runat="server" ID="btnActualizarDocente" OnClick="btnActualizarDocente_Click"/>
+</p>
+<p>
+    Buscar : <asp:TextBox runat="server" ID="txtBuscarDocente" />
+    <asp:Button Text="Buscar" runat="server" ID="btnBuscarDocente" OnClick="btnBuscarDocente_Click"/>
+</p>
+<p>
+    <asp:GridView runat="server" ID="gvDocente"></asp:GridView>
+</p></div>
+  <div class="tab-pane fade" id="simple-tabpanel-2" role="tabpanel" aria-labelledby="simple-tab-2"><h3>Mantimiento de la tabla Asignatura</h3>
+<p>
+    CodAsignatura : <asp:TextBox runat="server" ID="txtCodAsignatura" />
+</p>
+<p>
+    Asignatura : <asp:TextBox runat="server" ID="txtNombreAsignatura" />
+</p>
+<p>
+    CodRequisito : <asp:TextBox runat="server" ID="txtCodRequisito" />
+</p>
+<p>
+    <asp:Button Text="Agregar" runat="server" ID="btnAgregarAsignatura" OnClick="btnAgregarAsignatura_Click"/>
+    <asp:Button Text="Eliminar" runat="server" ID="btnEliminarAsignatura" OnClick="btnEliminarAsignatura_Click"/>
+    <asp:Button Text="Actualizar" runat="server" ID="btnActualizarAsignatura" OnClick="btnActualizarAsignatura_Click"/>
+</p>
+<p>
+    Buscar : <asp:TextBox runat="server" ID="txtBuscarAsignatura" />
+    <asp:Button Text="Buscar" runat="server" ID="btnBuscarAsignatura" OnClick="btnBuscarAsignatura_Click"/>
+</p>
+<p>
+    <asp:GridView runat="server" ID="gvAsignatura"></asp:GridView>
+</p></div>
+</div>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function () {
+            // Obtener el valor guardado del campo oculto
+            var activeTabIndex = document.getElementById('<%= hdnActiveTabIndex.ClientID %>').value;
+
+            // Si hay un valor, activar esa pestaña
+            if (activeTabIndex) {
+                setActiveTab(activeTabIndex);
+            }
+
+            // Al cambiar de pestaña, guardar el índice y cambiar las clases
+            var tabLinks = document.querySelectorAll('.nav-tabs a');
+            tabLinks.forEach(function (tab, index) {
+                tab.addEventListener('click', function () {
+                    document.getElementById('<%= hdnActiveTabIndex.ClientID %>').value = index;
+                    setActiveTab(index); // Cambiar la pestaña activa
+                });
+            });
+        });
+
+        function setActiveTab(index) {
+            // Eliminar 'active' y 'show' de todas las pestañas y contenido
+            document.querySelectorAll('.nav-tabs a').forEach(function (tab) {
+                tab.classList.remove('active');
+            });
+            document.querySelectorAll('.tab-pane').forEach(function (pane) {
+                pane.classList.remove('active', 'show');
+            });
+
+            // Añadir 'active' y 'show' solo a la pestaña y contenido seleccionados
+            document.querySelectorAll('.nav-tabs a')[index].classList.add('active');
+            document.querySelectorAll('.tab-pane')[index].classList.add('active', 'show');
+        }
+    </script>
+</asp:Content>
+
---
+++ CapaPresentacion/Intranet/frmIntranet.aspx.designer.cs
@@ -0,0 +1,314 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CapaPresentacion.Intranet
+{
+
+
+    public partial class frmIntranet
+    {
+
+        /// <summary>
+        /// Control hdnActiveTabIndex.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnActiveTabIndex;
+
+        /// <summary>
+        /// Control txtCodAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodAlumno;
+
+        /// <summary>
+        /// Control txtAPaternoAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAPaternoAlumno;
+
+        /// <summary>
+        /// Control txtAMaternoAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAMaternoAlumno;
+
+        /// <summary>
+        /// Control txtNombresAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombresAlumno;
+
+        /// <summary>
+        /// Control txtUsuarioAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuarioAlumno;
+
+        /// <summary>
+        /// Control txtCodCarrera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodCarrera;
+
+        /// <summary>
+        /// Control btnAgregarAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarAlumno;
+
+        /// <summary>
+        /// Control btnEliminarAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarAlumno;
+
+        /// <summary>
+        /// Control btnActualizarAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizarAlumno;
+
+        /// <summary>
+        /// Control txtBuscarAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBuscarAlumno;
+
+        /// <summary>
+        /// Control btnBuscarAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscarAlumno;
+
+        /// <summary>
+        /// Control gvAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvAlumno;
+
+        /// <summary>
+        /// Control txtCodDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodDocente;
+
+        /// <summary>
+        /// Control txtAPaternoDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAPaternoDocente;
+
+        /// <summary>
+        /// Control txtAMaternoDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAMaternoDocente;
+
+        /// <summary>
+        /// Control txtNombresDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombresDocente;
+
+        /// <summary>
+        /// Control txtUsuarioDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuarioDocente;
+
+        /// <summary>
+        /// Control btnAgregarDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarDocente;
+
+        /// <summary>
+        /// Control btnEliminarDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarDocente;
+
+        /// <summary>
+        /// Control btnActualizarDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizarDocente;
+
+        /// <summary>
+        /// Control txtBuscarDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBuscarDocente;
+
+        /// <summary>
+        /// Control btnBuscarDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscarDocente;
+
+        /// <summary>
+        /// Control gvDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvDocente;
+
+        /// <summary>
+        /// Control txtCodAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodAsignatura;
+
+        /// <summary>
+        /// Control txtNombreAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreAsignatura;
+
+        /// <summary>
+        /// Control txtCodRequisito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodRequisito;
+
+        /// <summary>
+        /// Control btnAgregarAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarAsignatura;
+
+        /// <summary>
+        /// Control btnEliminarAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarAsignatura;
+
+        /// <summary>
+        /// Control btnActualizarAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizarAsignatura;
+
+        /// <summary>
+        /// Control txtBuscarAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBuscarAsignatura;
+
+        /// <summary>
+        /// Control btnBuscarAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscarAsignatura;
+
+        /// <summary>
+        /// Control gvAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvAsignatura;
+    }
+}
